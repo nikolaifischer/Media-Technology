1538649d829e9db46438e27927fba142232694e2
Niki Fischer <nikolai.fischer@campus.lmu.de>
2017-01-21 11:22:44 +0100

    Audio Calendar working now
    - Everything is loaded async
    - Some clean up

--- public/js/controllers/AudioCtrl.js
+++ public/js/controllers/AudioCtrl.js
@@ -1,49 +1,86 @@
 angular.module('AudioCtrl', [])
     .controller('AudioController', function ($scope, $filter, $timeout, $log, $q, $http, $route, PlatformUser, Group, Lab, LabType, MaterialCalendarData, $window) {
-        //Get Audio LabType
-        $scope.audiolabs = LabType.find({
-            filter: {
-                where: {type: 1}
-            }
-        });
 
+        $scope.currentUser;
+        $scope.group;
+        $scope.labs;
         var groupedElements = {};
-        //Find Labs with Audio LabType Id
-        $scope.labs = Lab.find({
-            filter: {
-                where: {labTypeId: $scope.audiolabs.id}
-            }
-        }, function(labs) {
-            //Get format for calendar
-            labs.forEach(function(element) {
-                var index = element.date.slice(0, 10);
-                if(groupedElements[index] === undefined) {
-                    groupedElements[index] = [];
+        $scope.date = new Date();
+        $scope.time = new Date();
+        //$scope.dateTime = new Date();
+        $scope.minDate = moment().subtract(1, 'month');
+
+
+        /**
+         * Loads the labs from the DB and displays them on the calendar.
+         */
+        $scope.loadLabs = function () {
+            //Get Audio LabType
+            $scope.audiolabs = LabType.find({
+                filter: {
+                    where: {type: 1}
                 }
-                groupedElements[index].push(element);
-                //$scope.setDayContent(element.date.slice(0,10));
+            }, function (audiolabs) {
+
+                console.log(audiolabs);
+                // Find all Audio Labs
+                $scope.labs = Lab.find({
+                    filter: {
+                        where: {labTypeId: audiolabs.id}
+                    }
+                }, function (labs) {
+                    //Get format for calendar
+                    labs.forEach(function (element) {
+                        var date = element.date.slice(0, 10);
+                        if (groupedElements[date] === undefined) {
+                            groupedElements[date] = [];
+                        }
+                        groupedElements[date].push(element);
+
+                        for(var i =0; i<groupedElements.length; i++){
+                            console.log(groupedElements[i]);
+                        }
+                    });
+
+
+                    Object.keys(groupedElements).forEach(function (date) {
+                        MaterialCalendarData.setDayContent(new Date(date)," Termine verfügbar ");
+                    });
+
+
+                    return groupedElements;
+
+                }, function (error) {
+                    console.log(error);
+                });
+
+
             });
 
-            if (loadContentAsync) {
-                var deferred = $q.defer();
-                $timeout(function() {
-                    deferred.resolve(groupedElements);
-                });
-                return deferred.promise;
-            }
+        }
 
+        $scope.loadLabs();
 
-            return groupedElements;
+        //Find Labs with Audio LabType Id
 
-        }, function (error) {
-            console.log(error);
-        });
 
         // Get current User
-        $scope.currentUser;
+
         if (PlatformUser.isAuthenticated()) {
             PlatformUser.getCurrent(function (currentUser) {
                 $scope.currentUser = currentUser;
+                // This cannot be done with a filter, so it fetches all Groups
+                Group.find({}, function (groups) {
+                    // Success Callback
+                    for (var i = 0; i < groups.length; i++) {
+                        if (groups[i].groupMemberIds.indexOf($scope.currentUser.id) > -1) {
+                            $scope.group = groups[i];
+                        }
+                    }
+                }, function (error) {
+                    //Error Callback
+                    console.log(error);
+                });
 
             }, function (error) {
                 console.log(error);
@@ -51,19 +88,8 @@ angular.module('AudioCtrl', [])
         }
 
         // Find the Group the User is part of
-        $scope.group;
-        // This cannot be done with a filter, so it fetches all Groups
-        Group.find({}, function (groups) {
-            // Success Callback
-            for (var i = 0; i < groups.length; i++) {
-                if (groups[i].groupMemberIds.indexOf($scope.currentUser.id) > -1) {
-                    $scope.group = groups[i];
-                }
-            }
-        }, function (error) {
-            //Error Callback
-            console.log(error);
-        });
+
+
 
         // CALENDAR CODE
         $scope.dayFormat = "d";
@@ -89,82 +115,44 @@ angular.module('AudioCtrl', [])
             $scope.objects = (groupedElements[$scope.key] || []);
         };
 
-        $scope.prevMonth = function (data) {
-            $scope.msg = "You clicked (prev) month " + data.month + ", " + data.year;
-        };
-
-        $scope.nextMonth = function (data) {
-            $scope.msg = "You clicked (next) month " + data.month + ", " + data.year;
-        };
 
         $scope.tooltips = true;
 
-        var numFmt = function(num) {
-            num = num.toString();
-            if (num.length < 2) {
-                num = "0" + num;
-            }
-            return num;
-        };
 
-        var loadContentAsync = true;
 
-        //Set content in calendar cells
-        //TODO: load Content after find labs
-        $scope.setDayContent = function(date) {
-                var key;
-                if (date.length != undefined) {
-                    key = date;
-                } else {
-                    key = [date.getFullYear(), numFmt(date.getMonth() + 1), numFmt(date.getDate())].join("-");
-                }
-                var data = [];
-                var objects = (groupedElements[key] || []);
-                if(objects.length > 0) {
-                    for (var i =0; i <= objects.length && groupedElements[key][i] != undefined; i++){
-                        data.push(groupedElements[key][i].date.slice(11, 16));
-                    }
-                }
-            //$scope.dayContent = data;
-            /*if (loadContentAsync) {
-                var deferred = $q.defer();
-                $timeout(function() {
-                    deferred.resolve(data);
-                });
-                return deferred.promise;
-            }*/
-            return data;
-        };
-
-        $scope.date = new Date();
-        $scope.time = new Date();
-        //$scope.dateTime = new Date();
-        $scope.minDate = moment().subtract(1, 'month');
 
         $scope.createLab = function () {
             Lab.create({
-                    "date": $scope.dateTime,
-                    "duration": $scope.duration,
-                    "location": $scope.location,
-                    "labTypeId": $scope.audiolabs.id
-            }, function (error) {
+                "date": $scope.dateTime,
+                "duration": $scope.duration,
+                "location": $scope.location,
+                "labTypeId": $scope.audiolabs.id
+            }, function (lab) {
+                console.log(lab);
+
+                $scope.successMessage = "Praktikum wurde erstellt!";
+
+                // TODO: All the Labs are reloaded from the DB. This is pretty inefficent.
+                $scope.loadLabs();
+                $scope.dateTime = "";
+                $scope.duration = "";
+                $scope.location = "";
+
+            }, function(error){
                 console.log(error);
             });
-            $scope.successMessage = "Praktikum wurde erstellt!";
-            $scope.dateTime = "";
-            $scope.duration = "";
-            $scope.location = "";
+
         };
 
         $scope.priorities = [
-            { id: 0, name: '-' },
-            { id: 1, name: '1' },
-            { id: 2, name: '2' },
-            { id: 3, name: '3' }
+            {id: 0, name: '-'},
+            {id: 1, name: '1'},
+            {id: 2, name: '2'},
+            {id: 3, name: '3'}
         ];
         //$scope.selectedPriority = { id: 0, name: '-' };
         //TODO savePriorities
         /*$scope.savePriorities = function () {
-            console.log(this.selectedPriority);
-        }*/
+         console.log(this.selectedPriority);
+         }*/
     });
