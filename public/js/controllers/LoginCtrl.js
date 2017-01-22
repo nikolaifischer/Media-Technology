bd817d87dd201862caca8cf35c46ec6fe7f11d5f
Niki Fischer <nikolai.fischer@campus.lmu.de>
2017-01-22 18:59:46 +0100

    New Login
    - Better Design and User Flow
    - User doesn't have to type his name anymore

--- public/js/controllers/LoginCtrl.js
+++ public/js/controllers/LoginCtrl.js
@@ -1,42 +1,41 @@
-angular.module('LoginCtrl', ['ngMaterial' ]).controller('LoginController', function($scope, PlatformUser, $window,$mdToast) {
+angular.module('LoginCtrl', ['ngMaterial','ngMessages']).controller('LoginController', function ($scope, PlatformUser, $window, $mdToast) {
 
-    $scope.$root.hideNav=true;
+    $scope.$root.hideNav = true;
     /*
-    // if user is logged in, redirect to home
-    if ($window.sessionStorage.token != null){
-        $window.location.href = '/';
-    }
-    */
+     // if user is logged in, redirect to home
+     if ($window.sessionStorage.token != null){
+     $window.location.href = '/';
+     }
+     */
+    $scope.registerFlag = false;
+    $scope.whitelisted = true;
+    $scope.wrongLogin = false;
 
+    // name and first_name have to be set to a arbitrary String else the API will return an error. They are ignored.
     $scope.user = {
-        first_name: 'Kevin',
-        name: 'Dombrowski',
         email: 'kevin@campus.lmu.de',
         password: 'test',
-        repeatpassword: 'test'
+        repeatpassword: 'test',
+        first_name: 'This is ignored but has to be a String',
+        name: 'This is ignored but has to be a String'
     };
 
     $scope.registeredUser = {
-        email:"kevin@campus.lmu.de",
-        password:"test"
+        email: "kevin@campus.lmu.de",
+        password: "test"
     };
 
     $scope.login = function () {
         PlatformUser.login($scope.registeredUser,
-            function(response) {
+            function (response) {
                 console.log(response.id);
                 $window.sessionStorage.token = response.id;
                 $window.location.href = '/';
             },
-            function(errorResponse) {
+            function (errorResponse) {
                 console.log(errorResponse);
 
-                $mdToast.show(
-                    $mdToast.simple()
-                        .textContent('Falsche E-Mail/Passwort')
-                        .hideDelay(3000)
-                        .toastClass("toast")
-                );
+                $scope.wrongLogin = true;
 
                 delete $window.sessionStorage.token;
             }
@@ -44,24 +43,35 @@ angular.module('LoginCtrl', ['ngMaterial' ]).controller('LoginController', funct
     }
 
     $scope.register = function () {
-        if ($scope.user.password != $scope.user.repeatpassword){
-            // TODO: message to user and abort
-            console.log("Wrong Password")
+        if ($scope.user.password != $scope.user.repeatpassword) {
+            $mdToast.show(
+                $mdToast.simple()
+                    .textContent('Die Passwörter stimmen nicht überein')
+                    .hideDelay(3000)
+                    .toastClass("toast")
+            );
         }
+
 
         // TODO: how to check if user with email exists?
         PlatformUser.create($scope.user,
-            function(response) {
-            console.log(response);
+            function (response) {
                 $scope.registeredUser = {
                     email: response.email,
                     password: response.repeatpassword   // TODO: that's not nice...
                 };
                 $scope.login();
             },
-            function(errorResponse) {
-                console.log(errorResponse);
-                //delete $window.sessionStorage.token;
+            function (errorResponse) {
+
+                if (errorResponse.status == 403) {
+                    $scope.whitelisted=false;
+                }
+                else {
+
+                    console.log(errorResponse);
+                }
+
             }
         );
     };
