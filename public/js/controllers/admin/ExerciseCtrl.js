9bf4071befecae083b37cd17f55f36fc5b20683e
actioussan <eugenia.kovalevski@gmail.com>
2017-01-23 21:56:04 +0100

    exercises saved and deleted

--- public/js/controllers/admin/ExerciseCtrl.js
+++ public/js/controllers/admin/ExerciseCtrl.js
@@ -1,11 +1,11 @@
 angular.module('ExerciseCtrl', [])
-    .controller('ExerciseController', function ($location, $scope, $filter, Semester, PlatformUser, Exercise, $mdToast, $mdPanel, PendingPlatformUser) {
+    .controller('ExerciseController', function ($location, $scope, $filter, Semester, PlatformUser, Exercise, $mdToast, $mdPanel) {
 
         // TODO: diese Funktion wird bestimmt an vers. Stellen gebraucht, kann man die irgendwohin auslagern? Vll. in einen CommonFunctionsController oder so?
         $scope.getCurrentSemester = function() {
             var currDate = new Date();
 
-            return Semester.find({
+            return Semester.findOne({
                 filter: {
                     where: {
                         and: [
@@ -17,15 +17,13 @@ angular.module('ExerciseCtrl', [])
             })
         };
 
-        // TODO: Sa & So müssen raus (sind drin, weil einzige Übung in der DB ein Sonntag ist)
+
         $scope.weekSchedules = {
             "Montag": [],
             "Dienstag": [],
             "Mittwoch": [],
             "Donnerstag": [],
-            "Freitag": [],
-            "Samstag": [],
-            "Sonntag": []
+            "Freitag": []
         };
 
         $scope.semester = $scope.getCurrentSemester();
@@ -36,6 +34,7 @@ angular.module('ExerciseCtrl', [])
                 where: {semesterId: $scope.semester.id}
             }
         }, function(exercises) {
+            // group exercise dates to weekly exercises for display in schedule
             exercises.forEach(function(exercise) {
                 var dateObj = new Date(exercise.date);
                 var weekday = $filter('date')(dateObj, 'EEEE');
@@ -43,20 +42,20 @@ angular.module('ExerciseCtrl', [])
                 var endtime = dateObj.setTime(dateObj.getTime() + (1.5*60*60*1000));
                 var lectureEnd = $filter('date')(endtime, 'HH:mm');
 
-                var exerciseForTemplate = {
-                    name: exercise.name,
-                    location: exercise.location,
-                    daytime: daytime,
-                    lectureEnd: lectureEnd
-                };
-                // save time of the exercise (from date) in weekSchedules
-                $scope.weekSchedules[weekday].push(exerciseForTemplate);
+                if ($scope.weekSchedules[weekday].length <= 0 || $scope.weekSchedules[weekday].filter(function(e) { return e.daytime == daytime; }).length <= 0) {
+                    // add exercise to weekSchedules, if it's not in there already
+                    var exerciseForTemplate = {
+                        name: exercise.name,
+                        location: exercise.location,
+                        daytime: daytime,
+                        lectureEnd: lectureEnd
+                    };
+                    $scope.weekSchedules[weekday].push(exerciseForTemplate);
+                }
             });
-
         }, function (error) {
             console.log(error);
         });
-
 
 
         $scope.showDialog = function(weekday) {
@@ -89,13 +88,41 @@ angular.module('ExerciseCtrl', [])
         };
 
         $scope.removeExercise = function(weekday, index){
-            $scope.weekSchedules[weekday].splice(index, 1);
+            var exercise = $scope.weekSchedules[weekday].splice(index, 1)[0];
+
+            // get all weekday-time exercises of the scheduled weekday from now
+            var end = new Date($scope.semester.end_date).setHours(23, 59, 59, 0),
+                start = getExerciseStartDate(weekday),
+                hours = exercise.daytime.split(":")[0],
+                minutes = exercise.daytime.split(":")[1];
+
+            start.setHours(hours, minutes, 0, 0);
+
+            while(start <= end){
+                // save current start date because it gets faster updated than the exercise created...
+                var exerciseDate = new Date(start).toISOString();
+
+                Exercise.find({
+                    filter: {
+                        where: {date: exerciseDate}
+                    }
+                }, function(exercises) {
+                    // delete exercises
+                    exercises.forEach(function(exercise) {
+                        Exercise.deleteById({id: exercise.id}, function(response) {});
+                    });
+                }, function (error) {
+                    console.log(error);
+                });
+
+                start.setDate(start.getDate() + 7);
+            }
         };
 
     })
     .controller('PanelDialogCtrl', PanelDialogCtrl);
 
-function PanelDialogCtrl(mdPanelRef, $scope, weekSchedules, weekday, semester, Exercise) {
+function PanelDialogCtrl(mdPanelRef, $scope, $filter, weekSchedules, weekday, semester, Exercise) {
     this._mdPanelRef = mdPanelRef;
     $scope.weekSchedules = weekSchedules;
     $scope.weekday = weekday;
@@ -105,14 +132,14 @@ function PanelDialogCtrl(mdPanelRef, $scope, weekSchedules, weekday, semester, E
     $scope.exerciseTime = {};
 
     $scope.addExercise = function(weekday){
-        // TODO: für richtige Anzeige nach dem Speichern
-        var lectureEnd = "todo";
+        var lectureEnd = new Date($scope.exerciseTime[weekday]);
+        lectureEnd.setTime(lectureEnd.getTime() + (1.5*60*60*1000));
 
         var newExercise = {
             name: $scope.exerciseName[weekday],
             location: $scope.exerciseLocation[weekday],
-            daytime: $scope.exerciseTime[weekday],
-            lectureEnd: lectureEnd
+            daytime: $filter('date')($scope.exerciseTime[weekday], 'HH:mm'),
+            lectureEnd: $filter('date')(lectureEnd, 'HH:mm')
         };
 
         setTimeout(function(){
@@ -120,20 +147,31 @@ function PanelDialogCtrl(mdPanelRef, $scope, weekSchedules, weekday, semester, E
                 $scope.weekSchedules[weekday].push(newExercise);
             });
 
-            // TODO: weekSchedules in exercises umwandeln und erstellen
-            $scope.exercises = null;
-/*
-            // write new exercises to db
-            Exercise.create($scope.exercises,
-                function(response) {
-                    console.log(response);
+            // get all dates of the scheduled weekday from now
+            var end = new Date($scope.semester.end_date).setHours(23, 59, 59, 0),
+                start = getExerciseStartDate(weekday),
+                hours = $filter('date')($scope.exerciseTime[weekday], 'HH'),
+                minutes = $filter('date')($scope.exerciseTime[weekday], 'mm');
 
-                },
-                function(errorResponse) {
-                    console.log(errorResponse);
-                }
-            );
-*/
+            start.setHours(hours, minutes, 0, 0);
+
+            while(start <= end){
+                // save current start date because it gets faster updated than the exercise created...
+                var exerciseDate = new Date(start);
+
+                Exercise.create({
+                    "name": newExercise.name,
+                    "date": exerciseDate,
+                    "semesterId": $scope.semester.id,
+                    "location": newExercise.location
+                }, function(response) {
+                }, function(error) {
+                    console.log(error);
+                });
+
+                start.setDate(start.getDate() + 7);
+            }
+
             mdPanelRef.close();
         }, 300);
     };
@@ -147,3 +185,24 @@ PanelDialogCtrl.prototype.closeDialog = function() {
         panelRef.destroy();
     });
 };
+
+function getExerciseStartDate(weekday) {
+    var currentDay = new Date().getDay(),
+        weekdayNumbers = {"Montag":1, "Dienstag": 2, "Mittwoch": 3, "Donnerstag": 4, "Freitag": 5},
+        offset = 0;
+
+    if(currentDay == weekdayNumbers[weekday]) {
+        // if the next exercise would be today, jump to next week
+        offset = 7;
+    } else if(currentDay == 0) {
+        // if today is Sunday (day 0), offset is the desired exercise day
+        offset = weekdayNumbers[weekday];
+    } else {
+        offset = ((7 + weekdayNumbers[weekday]) - currentDay) % 7;
+    }
+
+    var start = new Date();
+    start.setDate(start.getDate() + offset);
+
+    return start;
+}
