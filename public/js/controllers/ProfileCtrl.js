94af7e64bf6d8198840056d66aa5afb2aa33fb11
Niki Fischer <nikolai.fischer@campus.lmu.de>
2017-02-04 22:32:10 +0100

    Fixing Password Change

--- public/js/controllers/ProfileCtrl.js
+++ public/js/controllers/ProfileCtrl.js
@@ -1,9 +1,11 @@
-angular.module('ProfileCtrl', ['ngMaterial' ]).controller('ProfileController', function($scope, PlatformUser, $window, $mdToast, $mdPanel) {
+angular.module('ProfileCtrl', ['ngMaterial']).controller('ProfileController', function ($scope, PlatformUser, $window, $mdToast, $mdPanel) {
 
-    $scope.currentUser;
+    //$scope.currentUser;
+    $scope.userId;
     if (PlatformUser.isAuthenticated()) {
         PlatformUser.getCurrent(function (currentUser) {
             $scope.currentUser = currentUser;
+            $scope.userId = currentUser.id;
         }, function (error) {
             console.log(error);
         })
@@ -11,31 +13,31 @@ angular.module('ProfileCtrl', ['ngMaterial' ]).controller('ProfileController', f
 
     // TODO: nicht mehr nötig, oder?
     /*
-    $scope.save = function () {
-        if (PlatformUser.isAuthenticated()) {
-            if ($scope.currentUser.first_name !== '') {
-                PlatformUser.prototype$updateAttributes({id: $scope.currentUser.id}, {first_name: $scope.currentUser.first_name},
-                    function() {
-                        $window.location.href = '/profile';
-                    },
-                    function(error) {
-                        console.log(error)
-                    });
-            }
-            if ($scope.currentUser.name !== '') {
-                PlatformUser.prototype$updateAttributes({id: $scope.currentUser.id}, {name: $scope.currentUser.name},
-                    function() {
-                        $window.location.href = '/profile';
-                    },
-                    function(error) {
-                        console.log(error)
-                    });
-            }
-        }
-    }
-    */
+     $scope.save = function () {
+     if (PlatformUser.isAuthenticated()) {
+     if ($scope.currentUser.first_name !== '') {
+     PlatformUser.prototype$updateAttributes({id: $scope.currentUser.id}, {first_name: $scope.currentUser.first_name},
+     function() {
+     $window.location.href = '/profile';
+     },
+     function(error) {
+     console.log(error)
+     });
+     }
+     if ($scope.currentUser.name !== '') {
+     PlatformUser.prototype$updateAttributes({id: $scope.currentUser.id}, {name: $scope.currentUser.name},
+     function() {
+     $window.location.href = '/profile';
+     },
+     function(error) {
+     console.log(error)
+     });
+     }
+     }
+     }
+     */
 
-    $scope.showPasswordDialog = function() {
+    $scope.showPasswordDialog = function () {
         var position = $mdPanel.newPanelPosition()
             .absolute()
             .center();
@@ -60,41 +62,43 @@ angular.module('ProfileCtrl', ['ngMaterial' ]).controller('ProfileController', f
     };
 
 
-
 }).controller('PasswortDialogCtrl', PasswortDialogCtrl);
 
-function PasswortDialogCtrl(mdPanelRef, $scope, $mdToast, PlatformUser) {
+function PasswortDialogCtrl(mdPanelRef, $scope, $mdToast, PlatformUser, $window) {
     this._mdPanelRef = mdPanelRef;
 
     $scope.changePassword = function () {
         // TODO: old password is not being checked (the only way to check it is to login the user with that pw, but is this necessary?)
         if (PlatformUser.isAuthenticated()) {
-            if ($scope.currentUser.newPassword !== $scope.currentUser.repeatNewPassword) {
-                // TODO: input error statt toast
-                $mdToast.show(
-                    $mdToast.simple()
-                        .textContent('Neues Passwort stimmmt nicht mit Wiederholung überein')
-                        .hideDelay(3000)
-                        .toastClass("toast")
-                );
-                return false;
-            }
+            PlatformUser.getCurrent(function (currentUser) {
+                if ($scope.currentUser.newPassword !== $scope.currentUser.repeatNewPassword) {
+                    // TODO: input error statt toast
+                    $mdToast.show(
+                        $mdToast.simple()
+                            .textContent('Neues Passwort stimmmt nicht mit Wiederholung überein')
+                            .hideDelay(3000)
+                            .toastClass("toast")
+                    );
+                    return false;
+                }
 
-            PlatformUser.prototype$updateAttributes({id: $scope.currentUser.id}, {password: $scope.currentUser.newPassword},
-                function() {
-                    $window.location.href = '/profile';
-                },
-                function(error) {
-                    console.log(error)
-                });
+
+                PlatformUser.prototype$updateAttributes({id: currentUser.id}, {password: $scope.currentUser.newPassword},
+                    function (success) {
+                        $window.location.href = '/profile';
+                    },
+                    function (error) {
+                        console.log(error)
+                    });
+            });
         }
     }
 }
 
-PasswortDialogCtrl.prototype.closePasswortDialog = function() {
+PasswortDialogCtrl.prototype.closePasswortDialog = function () {
     var panelRef = this._mdPanelRef;
 
-    panelRef && panelRef.close().then(function() {
+    panelRef && panelRef.close().then(function () {
         angular.element(document.querySelector('.change-password-dialog-open-button')).focus();
         panelRef.destroy();
     });
